d8a176510ad86d931c41f42e2d4e3db56220c048
Wim Spaargaren <wim_spaargaren@live.nl>
2023-07-05 16:43:45 +0200

    first program exercises

---
+++ 1.first-program/1.execute/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// Execute the following code; Both by compiling to a binary and without.
+func main() {
+	fmt.Println("Hello Go Training!")
+}
---
+++ 1.first-program/3.imports/main.go
@@ -0,0 +1,8 @@
+// Fix the issue in the following program, such that the main function is executed.
+package main
+
+
+
+func main() {
+	fmt.Println("Hello Go Training!")
+}
