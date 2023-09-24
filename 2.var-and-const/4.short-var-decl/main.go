eb238d6dce6b1a16f2b8290b0b50aa7c772a53b6
wimspaargaren <wim_spaargaren@live.nl>
2023-09-24 22:44:58 +0200

    clarify var exercises and clasrify testify exercise

---
+++ 2.var-and-const/4.short-var-decl/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Repeat exercise 1, but use short variable declarations instead.
+}
--- 6.testing/2.testify/add_test.go
+++ 6.testing/2.testify/add_test.go
@@ -4,5 +4,5 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	// Use the testify library to define assertions on the Add function.
-	// Leverage the assert package.
+	// Leverage the assert package: "github.com/stretchr/testify/assert"
 }
