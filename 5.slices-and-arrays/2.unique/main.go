b8a90fef11cdd3a71aa5aaad13a87778fd7c3968
wimspaargaren <wim_spaargaren@live.nl>
2023-09-25 14:58:24 +0200

    additional clarification for slice unique exercise

--- 5.slices-and-arrays/2.unique/main.go
+++ 5.slices-and-arrays/2.unique/main.go
@@ -2,5 +2,5 @@ package main
 
 func main() {
 	// Define a function Unique that takes a slice of integers and returns a new slice
-	// containing only unique integers
+	// containing only unique integers. The resulting slice should remain in the same order.
 }
