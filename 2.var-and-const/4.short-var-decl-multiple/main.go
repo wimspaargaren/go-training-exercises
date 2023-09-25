bd60904f51cb61e7d9eac7db423b317500784e6b
wimspaargaren <wim_spaargaren@live.nl>
2023-09-25 10:47:53 +0200

    correct var and const exercises

--- 2.var-and-const/4.short-var-decl-multiple/main.go
+++ 2.var-and-const/4.short-var-decl-multiple/main.go
@@ -2,8 +2,7 @@ package main
 
 func main() {
 	// Declare a string and boolean variable in a single line
-	// and print both variables.
+	// and print both variables. Use the short variable declaration.
 	// After the first print, change the content of the variables and
 	// print them again.
-
 }
