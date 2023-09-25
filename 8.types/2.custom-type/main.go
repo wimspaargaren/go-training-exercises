f8dcbf7329869749a8dc86ffebfdea4b7da9b458
wimspaargaren <wim_spaargaren@live.nl>
2023-09-25 18:19:23 +0200

    clarify types exercise 2

--- 8.types/2.custom-type/main.go
+++ 8.types/2.custom-type/main.go
@@ -4,7 +4,7 @@ type Currency string
 
 func main() {
 	// Create a custom "Currency" type which has a string as underlying type. Define three constant currency values: EUR, USD, GBP.
-	// Write a function that takes the Currency type as argument and tries to detect which currency we're dealing with.
+	// Write a function that takes the Currency type as argument and tries to detect which currency we're dealing with(hint: use a switch case).
 	// If we find a known currency we will format it with the corresponding currency sign (€, $, £).
 
 }
