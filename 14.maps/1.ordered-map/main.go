f5128afba86d9ef26ea11af6a2d6d6b0024a519d
wimspaargaren <wim_spaargaren@live.nl>
2023-09-27 13:54:52 +0200

    additional clarifications map exercises 1 & 2

--- 14.maps/1.ordered-map/main.go
+++ 14.maps/1.ordered-map/main.go
@@ -8,6 +8,7 @@ type OrderedMap interface {
 	Add(key string, value string)
 	// GetValuesOrdered returns the values of the ordered map in the same order as they were inserted.
 	// If a value with the same key was added twice, the ordering remains the same.
+	// Hint: Use a slice to store the keys in the order they were added.
 	GetValuesOrdered() []string
 }
 
--- 14.maps/2.word-counter/main.go
+++ 14.maps/2.word-counter/main.go
@@ -6,7 +6,7 @@ import (
 
 const input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-// Implement the function count words.
+// Implement the function count words. The function returns a map of words and their count.
 // Hint: splitting by whitespace and newline can be done using: strings.Fields
 func CountWords(input string) map[string]int {
 	// FIXME: Implement; Run the test to verify your solution.
