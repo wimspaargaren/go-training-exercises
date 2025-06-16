b824ae0de8a1f12e303938cff5864ea8c3d82765
Wim Spaargaren <wim_spaargaren@live.nl>
2025-06-16 10:55:20 +0200

    improve clarity chapter 02 exercise 03

--- 02.var-and-const/3.vars-vs-constants/main.go
+++ 02.var-and-const/3.vars-vs-constants/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// Fix the program, by only adjusting the next line (line 6)
+	// Fix the program, by only adjusting the next line (line 7)
 	const x int = 42
 	x = 21
 	fmt.Println("x", x)
