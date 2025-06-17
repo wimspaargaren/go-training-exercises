166d4ff7be547d121e9cd9c886b41d20fe55dcd5
Wim Spaargaren <wim_spaargaren@live.nl>
2025-06-17 11:31:48 +0200

    fix test case pointers exercise 3

--- 09.pointers/3.slice-pointers/main_test.go
+++ 09.pointers/3.slice-pointers/main_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestIncrSlice(t *testing.T) {
@@ -24,8 +25,9 @@ func TestIncrSlice(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
 			growSlice(test.Input)
-			for i, e := range *test.Input {
-				assert.Equal(t, test.Result[i], e)
+			require.Equal(t, len(test.Result), len(*test.Input), "length of slice should match")
+			for i, e := range test.Result {
+				assert.Equal(t, (*test.Input)[i], e)
 			}
 		})
 	}
