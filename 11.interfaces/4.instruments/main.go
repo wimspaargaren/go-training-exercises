a1cb52b6eaa0c273e195bd5dd8dc6c3e8e48b2da
Wim Spaargaren <wim_spaargaren@live.nl>
2025-09-30 13:37:02 +0200

    chore: add one additional exercise for interfaces in between http client & hello wolrd

---
+++ 11.interfaces/4.instruments/main.go
@@ -0,0 +1,27 @@
+package main
+
+// 1. Define an interface named Instrument that has one method: Play() string.
+
+// 2. Define a Guitar struct
+type Guitar struct {
+	// No fields needed for this example
+}
+
+// 3. Implement the Play method for Guitar
+
+// 2. Define a Piano struct
+type Piano struct {
+	// No fields needed for this example
+}
+
+// 3. Implement the Play method for Piano
+
+// 4. Create a perform function that plays the instrument.
+// func perform(i Instrument) {
+// 	// Your code here
+// }
+
+func main() {
+	// 5. Create instances and call perform
+	// your code here
+}
