b37ee7c2c5b8dcf8f986fc6ac1881c247f701601
Wim Spaargaren <wim_spaargaren@live.nl>
2026-02-16 21:14:01 +0100

    chore: small update referencing errors.AsType added in Go 1.26

--- 12.errors/2.test-error/main.go
+++ 12.errors/2.test-error/main.go
@@ -6,6 +6,7 @@ import (
 
 // Sqrt Copy your solution from the previous assignment and write a unit tests covering all branches and matching the
 // specific ErrNegativeSqrt result. Hint: you can leverage the assert.ErrorAs function from testify.
+// Bonus: see if you can leverage the new errors.AsType function added in Go 1.26. Note we'll cover generics later in the course.
 func Sqrt(x float64) (float64, error) {
 	return 0, nil
 }
